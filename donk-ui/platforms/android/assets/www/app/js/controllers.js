6a8e52f3d6b5f8d5b69391485fd42a0cf4af0a8a
Walter Miller <walter@waltermiller.ca>
2013-09-28 13:19:55 -0400

    Fixes nav bar and comments out cordova js for now

--- donk-ui/platforms/android/assets/www/app/js/controllers.js
+++ donk-ui/platforms/android/assets/www/app/js/controllers.js
@@ -12,9 +12,4 @@ angular.module('myApp.controllers', []).
   .controller('NavBarCtrl', function($scope, $location){
     $scope.location = $location;
 
-    $scope.$watch('location.path()', function (path) {
-      $scope.statusFilter = (path === '/active') ?
-        { completed: false } : (path === '/completed') ?
-        { completed: true } : null;
-    });
   });
