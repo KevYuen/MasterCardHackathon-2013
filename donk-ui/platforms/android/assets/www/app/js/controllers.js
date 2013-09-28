9919e3b67234e0e9c65777cb44ad45bcc9b87dc6
Walter Miller <walter@waltermiller.ca>
2013-09-28 13:09:13 -0400

    Adding in beginings of Angular stuff

--- donk-ui/platforms/android/assets/www/app/js/controllers.js
+++ donk-ui/platforms/android/assets/www/app/js/controllers.js
@@ -3,9 +3,18 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', function($scope) {
 
-  }])
-  .controller('MyCtrl2', [function() {
+  })
+  .controller('MyCtrl2', function($scope) {
 
-  }]);
+  })
+  .controller('NavBarCtrl', function($scope, $location){
+    $scope.location = $location;
+
+    $scope.$watch('location.path()', function (path) {
+      $scope.statusFilter = (path === '/active') ?
+        { completed: false } : (path === '/completed') ?
+        { completed: true } : null;
+    });
+  });
