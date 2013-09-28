01796bb31c62ecb4d1c2193e9f81383102304c2b
Sabeer Zaman <sabeer.zaman@gmail.com>
2013-09-28 16:18:23 -0400

    New trans: Attaches (placeholder) controller actions to form buttons.

--- donk-ui/www/app/js/controllers.js
+++ donk-ui/www/app/js/controllers.js
@@ -6,9 +6,32 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl1', function($scope) {
 
   })
-  .controller('TransNewCtrl', function($scope) {
 
+  .controller('TransNewCtrl', function($scope) {
+  	$scope.amount = '';
+  	$scope.description = '';
+
+  	// TODO: validate amount to be rounded to 2 decimal places
+
+  	$scope.save = function( e ) {
+  		console.log( 'Save clicked' );
+  		$scope.amount = ~~$scope.amount; // coerce into a number
+  		console.log( $scope.amount + ' for ' + $scope.description );
+  	};
+
+  	$scope.donk = function( e ) {
+  		console.log( 'Donk clicked' );
+  		$scope.amount = ~~$scope.amount; // coerce into a number
+  		console.log( $scope.amount + ' for ' + $scope.description );
+  	};
+
+  	$scope.cancel = function( e ) {
+  		console.log( 'Cancel clicked' );
+  		$scope.amount = '';
+  		$scope.description = '';
+  	};
   })
+
   .controller('NavBarCtrl', function($scope, $location){
     $scope.location = $location;
   })
