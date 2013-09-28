64fc4b8f0c30d329dcc7e3fe850d032c66412444
Kevin <KevinKYuen@gmail.com>
2013-09-28 11:33:53 -0400

    added initial user model

--- api/models/user.js
+++ api/models/user.js
@@ -1,1 +1,13 @@
-//model
+//user model
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var user = new Schema({
+	email: String,
+	cards: [{cardNumber: Number, expiryMonth: Number, expiryDate: Number, requestId:Number}],
+	geoLocation: Number,
+	password: String,
+	address: String
+});
+
+module.exports = mongoose.model('User', user);
