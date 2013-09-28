d64c8c6632ba75ce674ffb0ac8bf82b91aa5b61d
Walter Miller <walter@waltermiller.ca>
2013-09-28 14:50:49 -0400

    Removing platform directory

--- donk-ui/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\szaman\wamp\www\mchack\donk-ui\platforms\android\bin\AndroidManifest.xml : \
-C:\Users\szaman\wamp\www\mchack\donk-ui\platforms\android\AndroidManifest.xml \
--- donk-ui/platforms/android/bin/Donk-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\szaman\wamp\www\mchack\donk-ui\platforms\android\bin\Donk-debug-unaligned.apk : \
-C:\Users\szaman\wamp\www\mchack\donk-ui\platforms\android\bin\Donk.ap_ \
-C:\Users\szaman\wamp\www\mchack\donk-ui\platforms\android\bin\classes.dex \
-C:\Users\szaman\wamp\www\mchack\donk-ui\platforms\android\libs\cordova-3.0.0.jar \
