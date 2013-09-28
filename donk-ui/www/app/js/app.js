6983c574628c0b9fde18f1496ddc78e065c99b4f
Walter Miller <walter@waltermiller.ca>
2013-09-28 15:58:41 -0400

    Added simple history view

--- donk-ui/www/app/js/app.js
+++ donk-ui/www/app/js/app.js
@@ -6,6 +6,7 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/trans/new', {templateUrl: 'partials/trans_new.html', controller: 'TransNewCtrl'});
+
     $routeProvider.when('/view3', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/view1'});
   }]);
--- donk-ui/www/app/js/services.js
+++ donk-ui/www/app/js/services.js
@@ -5,5 +5,39 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+  .value('version', '0.1')
+  .factory('History', function(){
+
+    var service = {
+      purchases: [
+        {
+          amount: 350,
+          item: 'canoe',
+          buyer: 'Person name',
+          date: 'a date'
+        },
+        {
+          amount: 123,
+          item: 'T-Shirt',
+          buyer: 'Person name',
+          date: 'a date'
+        },
+        {
+          amount: 35,
+          item: 'Ice Cream',
+          buyer: 'Person name',
+          date: 'a date'
+        },
+        {
+          amount: 70,
+          item: 'Laptop',
+          buyer: 'Person name',
+          date: 'a date'
+        }
+      ]
+    };
+    console.log(service);
+    return service;
+
+  })
