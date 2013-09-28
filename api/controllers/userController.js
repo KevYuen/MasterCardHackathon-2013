70f2efd7d63a281843eb2c21b0bae2c1b6706374
Kevin <KevinKYuen@gmail.com>
2013-09-28 11:44:02 -0400

    added shell method for user controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,1 +1,22 @@
 //user controller
+var User = require('../models/user.js');
+
+/*
+ * login
+ * POST - /user/login
+ * server receive: {username: String, password: String}
+ * server send: {user}
+ */
+export.login = function(req,res){
+
+}
+
+/*
+ * create user
+ * POST - /user/create
+ * server receive: {email: String, password: String, address: String}
+ * server send: {executionMessage: “user created”}
+*/
+export.create = function(req,res){
+
+}
