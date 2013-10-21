522db49d447289f0d8662003535a3ab0c8da11af
Kevin <KevinKYuen@gmail.com>
2013-10-20 20:43:09 -0400

    added the other test suites

---
+++ rest-api-test/card.test.js
@@ -0,0 +1,33 @@
+var superagent = require('superagent'),
+	expect = require('expect.js'),
+	http = require('support/http');
+
+var serverUrl = "http://ec2-54-227-22-178.compute-1.amazonaws.com/";
+
+describe('express rest api server', function(){
+
+	before(function(done){
+    	done();
+  	});
+
+	beforeEach(function(done){
+		done();
+	});
+
+    it('GET / test if service is running', function(done){
+    superagent.get(serverUrl)
+        .end(function(e,res){
+            console.log(res.text);
+            expect(res.text).to.eql("Welcome to the Donk's API");
+            done();
+        });
+    });
+
+    afterEach(function(done){
+    	done();
+    });
+
+    after(function(done){
+    	done();
+    });
+});
